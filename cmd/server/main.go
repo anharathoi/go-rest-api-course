8991031f9dbcc318cb9fb2450afe6253433f540e
anharathoi <anharzihan@gmail.com>
2021-11-08 14:10:54 +1100

    Structuring application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointer to db connections
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up app")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go REST API")
+	app := App{}
+	if err := app.Run; err != nil {
+		fmt.Println("Error starting the API")
+		fmt.Println(err)
+	}
 }
